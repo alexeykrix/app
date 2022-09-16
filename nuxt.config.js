44d67815a4529f3fcf79f0fd12bf5768d4f5bae4
Alexey_Krix <54662775+alexeykrix@users.noreply.github.com>
2022-09-16 04:04:29 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/app/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
