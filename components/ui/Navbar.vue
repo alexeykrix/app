192b0019663cd1a8505c69bb32f7a31d4ad4b3d3
Alexey Krix <alexey@thefirstthelast.agency>
2022-09-16 03:15:26 +0300

    init

---
+++ components/ui/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="navbar">
+    <button class="navbar__btn btn">
+      <img class="btn__icon btn__icon-search" src="../../static/ic-search.svg" alt="">
+    </button>
+    <button class="navbar__btn btn">
+      <img class="btn__icon" src="../../static/ic-heart.svg" alt="">
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  height: 60px;
+  width: 100%;
+  background: #000;
+  border-top: 0.1px solid rgba(255, 255, 255, 0.5);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.btn {
+  padding: 5px;
+  height: 40px;
+  &__icon {
+    width: 30px;
+    height: 30px;
+    &-search {
+      width: 25px;
+      height: 25px;
+    }
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="main">
+    <SearchReults />
+    <Navbar />
+  </section>
+</template>
+
+<script>
+import SearchReults from '../components/SearchReults.vue';
+import Navbar from '../components/ui/Navbar.vue';
+export default {
+    name: "IndexPage",
+    components: { SearchReults, Navbar }
+}
+</script>
+<style lang="scss">
+@import "@/assets/main.scss";
+
+.main {
+  background: #000;
+  height: 100%;
+  overflow: hidden;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+</style>
